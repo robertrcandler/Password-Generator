f590e4a7fee71aec4e2245975eeabab6f3250a49
Robert Candler <robert.candler.32@gmail.com>
2020-03-19 23:33:43 -0600

    Got rid of cool boxes in favor of prompts, seriously the last version was way cooler

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  //these are my prompts 
+
+  //a prompt for how many characters the user would like
+    //must be 8-128 characters
+    var Characterprompt  = prompt("How many characters in the password, must be between 8 and 128.");
+    //make answer number type
+    var numCharacter = parseInt(Characterprompt);
+    //test for valid number of characters
+    for (isNaN(numCharacter)=true || numCharacter < 8 || numCharacter > 128) {
+      alert("Invalid Input, Please try again");
+      Characterprompt  = prompt("How many characters in the password, must be between 8 and 128.");
+      //make answer number type
+      numCharacter = parseInt(Characterprompt);
+    }
+    //store it in a variable
+
+  //ask the user if they want lowercase
+    //store it in a variable
+
+  //ask the user if they want uppercase
+    //store it in a variable
+
+  //ask the user if they want numeric
+    //store it in a variable
+
+  //ask the user if they want special characters
+    //store it in a variable
+}
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
